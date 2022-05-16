d902726278b055a2f0a9b69f60b6e6c2294c6e46
Alexander Chashkov <ozzyzulka@gmail.com>
2022-05-16 21:32:32 +0500

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+/**
+ * @author alex_chashkov
+ * @created 16/05/2022 - 21:30
+ * @project job4j
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
